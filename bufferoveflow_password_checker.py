9169d6ceedad0db981e164029ca83db866ea382e
Sahil0336 <mohamedsahil0336@outlook.com>
2022-11-16 18:18:16 +0530

    Final mod by self

--- bufferoveflow_password_checker.py
+++ bufferoveflow_password_checker.py
@@ -1,6 +1,6 @@
 l, u, p, d = 0, 0, 0, 0
 s = input("Enter a password: ")
-if (len(s) == 8):
+if (len(s) == 8 and s[0].isupper() == True and s[7].islower() == True):
     for i in s:
         if (i.islower()):
             l += 1
@@ -11,7 +11,11 @@ if (len(s) == 8):
         if (i == '@' or i == '$' or i == '_' or i == ','):
             p += 1
 else:
-    print('Password length must be 8')
+    print('''............................
+Password must be contain 8 characters
+First letter must be capital
+Last letter must be small
+Any one of the character should be alphanumeric''')
 if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l+p+u+d == len(s)):
     print("Valid password...")
 else:
