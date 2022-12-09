3913e7563c1cd97626c2814b69713aca438a8696
Sahil0336 <mohamedsahil0336@outlook.com>
2022-12-09 18:22:49 +0530

    user defined power method

---
+++ power_num_user_defined.py
@@ -0,0 +1,17 @@
+def power_of_num(number, power):
+
+    value = 1
+
+    for i in range(1, power+1):
+
+        value = value * number
+
+    return value
+
+
+number = int(input("Enter the number: "))
+
+power = int(input("Enter power: "))
+
+print(
+    f"value of {number} to the power {power} is  {power_of_num(number, power)}")
